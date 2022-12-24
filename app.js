49b9f11e1734a2211ef4054ac1ab54cf8ff319b8
Hazael Lopez <elhazael134@gmail.com>
2022-12-23 21:12:50 -0600

    Request y Response

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.write('Hola mundo...');
+
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+
+    // const persona = {
+    //     id: 'ADF12',
+    //     nombre: 'Hazael'
+    // }
+
+    res.write('id, nombre\n');
+    res.write('1, Hazael\n');
+    res.write('2, Adriana\n');
+    res.write('3, Dany\n');
     res.end();
 })
 .listen(8080);
