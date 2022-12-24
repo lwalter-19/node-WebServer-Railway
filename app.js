7210707b739ba10362c3a1a6bd581f907a8a4377
Hazael Lopez <elhazael134@gmail.com>
2022-12-24 10:27:25 -0600

    Servir contenido estático

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+// Servir el contenido estatico
+app.use(express.static('public'));
 
 app.get('/hola-mundo', (req, res) => {
   res.send('Hola mundo en su respectiva ruta')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 Page not found')
+    res.sendFile(__dirname + '/public/404.html')
   });
 
-app.listen(8080, () =>{
-    console.log('Aplicación corriendo en el puerto:', 8080)
+app.listen(port, () =>{
+    console.log(`Aplication runing in Port: ${port}`)
 });
